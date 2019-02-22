38fe6aea84bf71a5a610f46c00f40b47b89f0e36
ankitac3021 <47849024+ankitac3021@users.noreply.github.com>
2019-02-22 17:51:44 +0530

    Add files via upload

---
+++ AdoptionWebsite/script.js
@@ -0,0 +1,22 @@
+//variables
+/* var a="ankita";
+console.log(a); //writing variables 
+var age=20;
+console.log(age);
+var x;
+console.log(x);
+
+//type coercion
+console.log(a+'is'+age+'years old');
+*/
+
+var johnmass,johnheight,markheight,markmass;
+johnmass=40;
+markmass=60;
+johnheight=161;
+markheight=171;
+var johnbmi,markbmi;
+johnbmi=johnmass/(johnheight*johnheight);
+markbmi=markmass/(markheight*markheight);
+var compare=johnbmi>markbmi;
+console.log('Is BMI of John greater than Mark? '+compare);
